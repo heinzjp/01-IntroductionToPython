3b103b47cb485b503a44bd49961344de639c914e
heinzjp <heinzjp@rose-hulman.edu>
2018-11-26 09:50:07 -0500

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Justin Heinz.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,23 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+justin = rg.SimpleTurtle()
+justin.pen = rg.Pen('blue', 30)
+justin.speed = 8
+ashley = rg.SimpleTurtle()
+ashley.pen = rg.Pen('red', 30)
+
+justin.forward(200)
+ashley.backward(200)
+size = 200
+
+for k in range(3):
+    justin.draw_circle(size)
+    ashley.draw_square(size)
+    size = size - 50
+
+window.close_on_mouse_click()
